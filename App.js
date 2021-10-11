a30f5c14766fbe394fdf46310ec2632f8dd86478
Jean Molino <jean.molino@zallpy.com>
2021-10-11 20:11:37 -0300

    add eslint

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import {
+  Button, StyleSheet, Text, View,
+} from 'react-native';
 
 export default function App() {
-
   return (
     <View style={styles.container}>
       <Text>Olá mundo</Text>
